9b2e77906cb6ed0c23622acd82f6bf0177842229
anggerenn <anggernolnem@gmail.com>
2021-05-06 22:45:53 +0700

    update about

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,7 @@ const About = () => {
   return (
     <>
       <h1 className="text-xl font-semibold text-center">About</h1>
-      <p className="mt-2">
-        This is an SPA for CRUD operations built using NextJS.
-      </p>
+      <p className="mt-2">This is an SPA built using NextJS.</p>
       <a
         className="text-sm text-gray-500 border-b-2 border-gray-500 border-dotted hover:text-gray-900 hover:border-gray-900"
         target="_blank"
